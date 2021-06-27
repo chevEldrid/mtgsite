9dbf1fa56bf7a287d88411f46fcca5d06f51b085
chevEldrid <chev424@gmail.com>
2021-06-26 18:07:57 -0700

    updates to decklist script and new vid

--- _scripts/deckGenerator.py
+++ _scripts/deckGenerator.py
@@ -19,12 +19,10 @@ HTML_CONTAINER = '<div class="row">\n\t<div class="col-md-2"></div>\n<div class=
 HTML_COLUMN = '<div class="col-6">{0}\n</div>'
 HTML_TYPE = '<b>{0}</b>\n<p class="mb-0">\n{1}</p>\n'
 
-TYPES = ["COMMANDER", "COMPANION", "COMMANDERS", "SORCERIES", "CREATURES",
-         "INSTANTS", "ARTIFACTS", "ENCHANTMENTS", "PLANESWALKERS", "LANDS", "UNKNOWN"]
 CLASSES = ["Commander", "Sorcery", "Instant", "Artifact", "Creature",
            "Planeswalker", "Land", "Enchantment", "Unknown"]
-TYPES_TWO = ["Commanders", "Sorceries", "Instants", "Artifacts", "Creatures",
-             "Planeswalkers", "Lands", "Enchantments", "Unknowns"]
+TYPES = ["Commanders", "Sorceries", "Instants", "Artifacts", "Creatures",
+         "Planeswalkers", "Lands", "Enchantments", "Unknowns"]
 cards = {}
 card_groups = []
 
@@ -85,20 +83,39 @@ def getCardType(name):
     return card_type
 
 
-def cardnameAndCount(cardline):
+# def cardnameAndCount(cardline):
+#     words = cardline.split()
+#     cardName = []
+#     cardCount = 1
+#     for word in words:
+#         if bool(re.search(r'\d', word)):
+#             cardCount = ''.join([i for i in word if i.isdigit()])
+#             continue
+#         if "*" in word:
+#             continue
+#         cardName.append(word)
+#     return [" ".join(cardName), cardCount]
+
+def getCardDetails(cardline):
     words = cardline.split()
-    cardName = []
+    nameWords = []
     cardCount = 1
+    isCommander = False
+    cardType = "Unknown"
     for word in words:
         if bool(re.search(r'\d', word)):
             cardCount = ''.join([i for i in word if i.isdigit()])
             continue
-        cardName.append(word)
-    return [" ".join(cardName), cardCount]
+        if "*" in word:
+            isCommander = True
+            continue
+        nameWords.append(word)
+    cardName = " ".join(nameWords)
+    cardType = getCardType(cardName)
+    return [cardName, cardCount, cardType, isCommander]
+
 
 # count the number of cards of each type from input file
-
-
 def tally_card_types(inputfile):
     cardCount = 0
     input_file = open(inputfile, "r")
@@ -106,12 +123,15 @@ def tally_card_types(inputfile):
     for line in input_file:
         cur_word = line.rstrip()
 
-        cardLineDetails = cardnameAndCount(cur_word)
-        cardType = getCardType(cardLineDetails[0])
+        # cardLineDetails = cardnameAndCount(cur_word)
+        # cardType = getCardType(cardLineDetails[0])
+        cardDetails = getCardDetails(cur_word)
+        cardType = cardDetails[2]
         if cardType not in cards.keys():
-            cards[cardType] = CardType(TYPES_TWO[CLASSES.index(cardType)])
+            cards[cardType] = CardType(TYPES[CLASSES.index(cardType)])
 
-        cards[cardType].add_card(Card(cardLineDetails))
+        # cards[cardType].add_card(Card(cardLineDetails))
+        cards[cardType].add_card(Card(cardDetails))
         cardCount += 1
     input_file.close()
     return cardCount
@@ -146,9 +166,8 @@ def generateEvenColumns(halfDeck, memo):
             otherSet.append(x)
     return [subset, otherSet]
 
+
 # formating methods
-
-
 def formatCardType(cardType):
     formattedCards = []
     for card in cardType.cards:
@@ -218,12 +237,6 @@ def main(argv):
         halfDeck -= 1
         memo = dict()
         cardsSubset = generateEvenColumns(halfDeck, memo)
-
-    # for card in cardsSubset[0]:
-    #     print(card.type + ": " + str(card.count()))
-    # print("...and for the other side...")
-    # for card in cardsSubset[1]:
-    #     print(card.type + ": " + str(card.count()))
 
     # attempt at creating proper formatted list - leggo
     decklist = formatContainer(cardsSubset[0], cardsSubset[1])
--- _scripts/site_publish.py
+++ _scripts/site_publish.py
@@ -13,14 +13,39 @@ def string_contained(file_name, string_to_search):
                 return True
     return False
 
+
+def sanityCheckConfirmation():
+    noAnswer = True
+    confirmed = False
+
+    while noAnswer:
+        print("Just for sanity's sake, did you....[y/n]")
+        print("- Remember to update the media.yml with any new videos released?")
+        confirmation = str(input())
+        if confirmation[0].upper() == "Y":
+            print("Excellent, we proCEED!")
+            noAnswer = False
+            confirmed = True
+        elif confirmation[0].upper() == "N":
+            print("Get on that ish!!")
+            noAnswer = False
+        else:
+            print("input not understood, we try again...")
+    return confirmed
+
+
 def main():
     print("The low budget file shuffling cousin!")
     source = '../_site/'
-    PRODUCTION_TEST_STRING = 'UA-180984713-1' #ID for google analytics, should only be present in production
+    # ID for google analytics, should only be present in production
+    PRODUCTION_TEST_STRING = 'UA-180984713-1'
     # check if the site directory is with a production build...
     site_index_file = source+'index.html'
     if not string_contained(site_index_file, PRODUCTION_TEST_STRING):
         print('Production test string was not found in _site index file...this might be a dev build\nExiting.')
+        exit(0)
+    if not sanityCheckConfirmation():
+        print('Publishing Script exiting, fix the sanity check to continue')
         exit(0)
     # clear out old hexdrinkers directory
     SITE_DIRECTORY = '../../hexdrinkers'
@@ -28,11 +53,11 @@ def main():
     for filename in os.listdir(SITE_DIRECTORY):
         file_path = os.path.join(SITE_DIRECTORY, filename)
         try:
-            if filename != '.gitignore': #special exception to keep the one file we need
+            if filename != '.gitignore':  # special exception to keep the one file we need
                 if os.path.isfile(file_path) or os.path.islink(file_path):
                     os.unlink(file_path)
                 elif os.path.isdir(file_path):
-                    if filename != ".git": #can't get rid of git...
+                    if filename != ".git":  # can't get rid of git...
                         shutil.rmtree(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
@@ -44,6 +69,7 @@ def main():
         shutil.move(source+f, dest1)
 
     print("Site moved successfully!")
+
 
 if __name__ == '__main__':
     main()
